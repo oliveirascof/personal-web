ed3c344b1df5ec32b342ab3903407593c81480aa
oliveirascof <oliveirascof@gmail.com>
2022-09-27 00:20:33 -0300

    home page check commit

--- up.push.git.py
+++ up.push.git.py
@@ -5,34 +5,48 @@ import pyautogui as py
 
 # Abre o cmd e inicializa o repo
 git_init = os.popen("git init").read()
+print("Inicializando repositorio")
 print(git_init)
+print("OK")
 sl(1)
+
 
 # Adiciona os arquivos modificados na lista para commit
 git_add = os.popen("git add -A").read()
+print("Adicionando itens na lista para o commit")
 print(git_add)
+print("OK")
 sl(1)
 
+
 # Altera para branch main
+print("Checando a branch e setando para main")
 os.popen("git branch -M main")
-sl(1)
 branch_main = os.popen("git branch").read()
 print(branch_main)
+print("OK")
 sl(1)
+
 
 commit = py.prompt("Digite a mensagem para o commit")
 
 # Realiza o commit
+print("Realizando o commit...")
 commit = os.popen(f'git commit -m "{commit}"').read()
 print(commit)
+print("OK")
 sl(1)
+
 
 py.alert("Commit feito! Agora vamos subir os arquivos para o Github")
 
 # Realiza o push para o repositorio no Github
+print("Fazendo o push para o github")
 git_push = os.popen("git push -f origin main").read()
 print(git_push)
+print("OK")
 sl(1)
+
 
 py.alert("Finalizado! Arquivos estão no git")
 
