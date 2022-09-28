46e056d3b15ec12e39fed46e0a0f331f94664bf4
Felipe Oliveira <oliveirascof@gmail.com>
2022-09-28 12:35:53 -0300

    daily check

--- push.git.py
+++ push.git.py
@@ -7,43 +7,32 @@ from tkinter import messagebox as mb
 
 # Abre o cmd e inicializa o repo
 git_init = popen("git init").read()
-print("Inicializando repositorio")
-print(git_init)
-print("OK")
-
+alert("Inicializando repositorio")
 
 # Adiciona os arquivos modificados na lista para commit
 git_add = popen("git add -A").read()
-print("Adicionando itens na lista para o commit")
-print(git_add)
-print("OK")
-
+alert("Adicionando itens na lista para o commit")
 
 # Altera para branch main
-print("Checando a branch e setando para main")
 popen("git branch -M main")
 branch_main = popen("git branch").read()
-print(branch_main)
-print("OK")
-
+alert(f"Branch -> {branch_main}")
 
 commit = prompt("Digite a mensagem para o commit")
-# Realiza o commit
-comm = mb.askyesno('Commit', 'Vamos fazer o commit?')
+comm = mb.askyesno('Commit', 'Proceder com o commit?')
 if not comm:
     exit()
-
+    
+# Realiza o commit
 commit = popen(f'git commit -m "{commit}"').read()
 
 # Realiza o push para o repositorio no Github
-sair = mb.askyesno('Commit feito', 'Deseja subir para o Git?')
+sair = mb.askyesno('Commit feito', 'Deseja subir os arquivos para o Git?')
 if not sair:
     exit()
 
 git_push = popen("git push -f origin main").read()
-print(git_push)
-print("OK")
-
+alert(git_push)
 
 alert("Finalizado!")
 exit()
