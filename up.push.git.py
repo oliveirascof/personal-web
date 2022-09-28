9bcefda8dc71589063a01015282e77c5dd3ab08c
Felipe Oliveira <oliveirascof@gmail.com>
2022-09-28 09:31:57 -0300

    daily check

--- up.push.git.py
+++ up.push.git.py
@@ -1,23 +1,22 @@
-import sys
-import os
-from time import sleep as sl
-import pyautogui as py
+from sys import exit
+from os import popen
+from time import sleep
+from pyautogui import alert
+from pyautogui import prompt
 
 # Abre o cmd e inicializa o repo
 git_init = os.popen("git init").read()
 print("Inicializando repositorio")
 print(git_init)
 print("OK")
-sl(1)
-
+sleep(1)
 
 # Adiciona os arquivos modificados na lista para commit
 git_add = os.popen("git add -A").read()
 print("Adicionando itens na lista para o commit")
 print(git_add)
 print("OK")
-sl(1)
-
+sleep(1)
 
 # Altera para branch main
 print("Checando a branch e setando para main")
@@ -25,36 +24,25 @@ os.popen("git branch -M main")
 branch_main = os.popen("git branch").read()
 print(branch_main)
 print("OK")
-sl(1)
+sleep(1)
 
-
-commit = py.prompt("Digite a mensagem para o commit")
+commit = prompt("Digite a mensagem para o commit")
 
 # Realiza o commit
 print("Realizando o commit...")
-commit = os.popen(f'git commit -m "{commit}"').read()
+commit = popen(f'git commit -m "{commit}"').read()
 print(commit)
 print("OK")
-sl(1)
+sleep(1)
 
-
-py.alert("Commit feito! Agora vamos subir os arquivos para o Github")
+alert("Commit feito! Agora vamos subir os arquivos para o Github")
 
 # Realiza o push para o repositorio no Github
 print("Fazendo o push para o github")
-git_push = os.popen("git push -f origin main").read()
+git_push = popen("git push -f origin main").read()
 print(git_push)
 print("OK")
-sl(1)
+sleep(1)
 
-
-py.alert("Finalizado! Arquivos estão no git")
-
-
-
-
-
-
-
-    
-sys.exit()
+alert("Finalizado! Arquivos estão no git")
+exit()
